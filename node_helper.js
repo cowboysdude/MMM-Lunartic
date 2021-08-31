9ac3f90db54b0cc17419692a65728b1a18932627
John Wade <johnw8017@gmail.com>
2021-08-31 07:11:59 -0400

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -5,8 +5,7 @@
  *
  */
 const NodeHelper = require('node_helper');
-const request = require('request');
-
+const axios = require('axios');
 
 module.exports = NodeHelper.create({
 
@@ -14,44 +13,57 @@ module.exports = NodeHelper.create({
         console.log("Starting node_helper for: " + this.name);
     },
 
+getLunartic: function(url) {
+    var self = this;
+    axios.get('https://mykle.herokuapp.com/moon')
+        .then(function(response) {
+            var info = response.data;
+            var data = [];
+            // set vars
+            var dfcoe = info.DFCOE;
+            var dfs = info.DFS;
+            var fm = info.FM.UT;
+            var nnm = info.NNM.UT;
+            var age = info.age;
+            var ill = info.illumination;
+            var stage = info.stage;
 
-    getLunartic: function(url) {
-        var self = this;
-        var data =[];
-        request({
-            url: "https://mykle.herokuapp.com/moon",
-            method: 'GET'
-        }, (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                var info = JSON.parse(body);
-//                console.log(info); // for checking
+            // Make each into an object so they can be put into a rotation courtesy of @cowboysdude
+            dfcoe = {
+                dfcoe
+            };
+            dfs = {
+                dfs
+            };
+            fm = {
+                fm
+            };
+            nnm = {
+                nnm
+            };
+            age = {
+                age
+            };
+            ill = {
+                ill
+            };
+            stage = {
+                stage
+            };
 
-                // create array
-                var data = [];
-                // set vars
-                var dfcoe  =  info.DFCOE ;
-                var dfs  =  info.DFS;
-                var fm  = info.FM.UT;
-                var nnm  = info.NNM.UT;
-                var age  = info.age;
-                var ill  = info.illumination;
-                var stage  = info.stage;
-
-                // Make each into an object so they can be put into a rotation courtesy of @cowboysdude
-                dfcoe  =  {dfcoe};
-                dfs = {dfs};
-                fm = {fm};
-                nnm = {nnm};
-                age = {age};
-                ill = {ill};
-                stage = {stage};
-
-                data.push(dfcoe,dfs,fm,nnm,age,ill,stage); // push the data
-//               console.log(response.statusCode + data); // for checking
-                this.sendSocketNotification('LUNARTIC_RESULT', data);
-            }
+            data.push(dfcoe, dfs, fm, nnm, age, ill, stage); // push the data
+            //              console.log(response.statusCode + data); // for checking
+            self.sendSocketNotification('LUNARTIC_RESULT', data);
+        })
+        .catch(function(error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function() {
+            // always executed
+            console.log("Done");
         });
-    },
+}, 
 
 
     socketNotificationReceived: function(notification, payload) {
